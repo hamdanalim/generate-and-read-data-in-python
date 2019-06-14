ff26e43cf357cca74d9f53baac302ce83bb43ae2
Hamdan <hamdanali.mutaqin@gmail.com>
2019-06-14 19:10:45 +0700

    fix(config): Penambahan Jumlah Data Array Sebanyak 4800000

--- generate_data.py
+++ generate_data.py
@@ -13,14 +13,14 @@ import uuid
 from random import randrange
 import csv
 
-dft = pd.DataFrame({'id' : [uuid.uuid4() for i in range(480)],
+dft = pd.DataFrame({'id' : [uuid.uuid4() for i in range(4800000)],
                     'device_id' : ['00010','00011','00012','00013','00014','00015','00016','00017',
-                                   '00018','00019','0001A','0001B','0001C','0001D','0001E','0001F'] * 30,
+                                   '00018','00019','0001A','0001B','0001C','0001D','0001E','0001F'] * 300000,
                    #'device_id' : [for i in range(120)],
-                   'username' : ['Andi', 'Budi', 'Taja'] * 160,
-                   'lokasi' : ['Bandung', 'Jakarta'] * 240,
-                    'amount' : [np.random.randint(10, 1000) for i in range(480)],
-                    'timestamp' : [np.random.choice(pd.date_range(datetime.datetime(2019,1,1),datetime.datetime(2019,12,30))) for i in range(480)] 
+                   'username' : ['Andi', 'Budi', 'Taja'] * 1600000,
+                   'lokasi' : ['Bandung', 'Jakarta'] * 2400000,
+                    'amount' : [np.random.randint(10, 1000) for i in range(4800000)],
+                    'timestamp' : [np.random.choice(pd.date_range(datetime.datetime(2019,1,1),datetime.datetime(2019,12,30))) for i in range(4800000)] 
                     #'time' : [[date.strftime('%H:%M:%S') for date in pd.date_range(start='2017-01-01', end='2017-01-04', periode=120)]]
                    })
 dft.to_csv('dft2.txt', index=False)
